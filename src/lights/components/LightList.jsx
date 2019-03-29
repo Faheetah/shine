1f86bad109dd8fa12b7bada5d5782417cd19fb30
Faheetah <Faheetah@users.noreply.github.com>
2019-03-29 12:02:47 -0500

    huh

--- src/lights/components/LightList.jsx
+++ src/lights/components/LightList.jsx
@@ -5,9 +5,7 @@ import Light from './Light'
 const LightList = ({lights}) => {
   return (
     <div>
-      {Object.keys(lights).map((light, i) => 
-        <Light key={i} light={lights[light]} id={light} />
-      )}
+      <div>SEARCH</div>
     </div>
   )
 }
